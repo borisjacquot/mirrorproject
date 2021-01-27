962666df7e06be27e230338802030b51830e5c19
Disnos <titouan_64@hotmail.fr>
2021-01-27 15:21:14 +0100

    [CALENDAR] Indentation du programme

--- modules/news_module.js
+++ modules/news_module.js
@@ -1,6 +1,6 @@
 var Feed = require('rss-to-json');
 var schedule = require('node-schedule');
-var article = 0; 
+var article = 0;
 
 var j = schedule.scheduleJob('*/3 * * * * *', function(){
   Feed.load('https://lemonde.fr/rss/une.xml', function(err, rss){
