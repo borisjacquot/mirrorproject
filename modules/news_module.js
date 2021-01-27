9ec1f2a53dc2dd2cf918333aee2d3584bd80c4cb
Disnos <titouan_64@hotmail.fr>
2021-01-27 15:01:54 +0100

    [NEWS] Ajout du rafraichissement de la news affichée toutes les 3 minutes

--- modules/news_module.js
+++ modules/news_module.js
@@ -1,9 +1,13 @@
 var Feed = require('rss-to-json');
+var schedule = require('node-schedule');
+var article = 0; 
 
-Feed.load('https://lemonde.fr/rss/une.xml', function(err, rss){
-	    const truc = JSON.stringify(rss, null, 3);
-	    const obj = JSON.parse(truc);
-	    console.log(obj.items[0].title);
-	    console.log(obj.items[0].description);
-	    // il faudra ajouter un changement d'article toutes les 5 minutes (indice de items)
+var j = schedule.scheduleJob('*/3 * * * *', function(){
+  Feed.load('https://lemonde.fr/rss/une.xml', function(err, rss){
+    const truc = JSON.stringify(rss, null, 3);
+    const obj = JSON.parse(truc);
+    console.log(obj.items[article].title);
+    console.log(obj.items[article].description);
+  });
+  article = (article + 1) % 4;
 });
