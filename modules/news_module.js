f65fe40540de804e0ac14129c3297d75f78dbea2
Disnos <titouan_64@hotmail.fr>
2021-01-27 15:10:59 +0100

    [CALENDRIER] Ajout du rafraichissement du calendrier toutes les heures

--- modules/news_module.js
+++ modules/news_module.js
@@ -2,12 +2,10 @@ var Feed = require('rss-to-json');
 var schedule = require('node-schedule');
 var article = 0; 
 
-var j = schedule.scheduleJob('*/3 * * * *', function(){
+var j = schedule.scheduleJob('*/3 * * * * *', function(){
   Feed.load('https://lemonde.fr/rss/une.xml', function(err, rss){
-    const truc = JSON.stringify(rss, null, 3);
-    const obj = JSON.parse(truc);
-    console.log(obj.items[article].title);
-    console.log(obj.items[article].description);
+    console.log(rss.items[0].title);
+    console.log(rss.items[0].description);
   });
   article = (article + 1) % 4;
 });
